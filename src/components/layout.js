e08ee5ab8038730e6dcf1ae85ebe8bc0700a275d
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-04-19 18:03:13 -0300

    Remove unused files

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,19 +13,8 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
@@ -39,9 +28,6 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
     </>
