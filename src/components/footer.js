3b37bdfb4d491f877272d83e11ca06917052205f
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-04-19 18:29:20 -0300

    Inclusão do Tailwind e arquivos base

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const Footer = () => (
+    <footer>
+        <p>Developed by <a href="https://danilovilhena.com">Danilo Vilhena</a></p>
+    </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,23 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
+import PlusIcon from "../icons/plus.svg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Index Page</h1>
+    <h1>do what?</h1>
+    <h2>No register. No login. Not a bunch of tutorials.</h2>
+    <h2>Just write your daily tasks and keep track of them.</h2>
+    <div>
+      <input type="text" id="fname" name="fname" placeholder="What do I need to do today?" />
+      <button type="button">
+        Add task
+        <img src={PlusIcon} aria-hidden/>
+      </button>
+    </div>
+    <Footer />
   </Layout>
 )
 
